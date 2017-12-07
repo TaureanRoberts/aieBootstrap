620367aa66503ddcf90b5302f49c57a9ae2413f3
Taurean Roberts <taureanjroberts@gmail.com>
2017-12-06 19:02:45 -0600

    Finishing up documentation for bootstrap in class

--- project2D/Entity.h
+++ project2D/Entity.h
@@ -4,28 +4,28 @@
 class Entity
 {
 public:
-	//Prototype:
-	//Description:
-	//Precondition:
-	//Post Condition:
-	//Protection Level:
+	//Prototype: Entity()
+	//Description: Handles the Vector/Matrix classes when its child class is called
+	//Precondition: handles the Vector/Matrix classes
+	//Post Condition: returns places for used calls
+	//Protection Level: Public
 	Entity();
-	//Prototype:
-	//Description:
+	//Prototype: ~Entity()
+	//Description: deconstructs the information given on in the default
 	//Precondition:
 	//Post Condition:
-	//Protection Level:
+	//Protection Level: Public
 	~Entity();
-	//Prototype:
-	//Description:
-	//Precondition:
-	//Post Condition:
-	//Protection Level:
+	//Prototype: Vector2 mScale
+	//Description: Sets the size of a object or item
+	//Precondition: takes in a X and Y scale to size upon
+	//Post Condition: Returns its size given
+	//Protection Level: Public
 	Vector2 mScale;
-	//Prototype:
-	//Description:
-	//Precondition:
-	//Post Condition:
-	//Protection Level:
+	//Prototype: Vector2 mPos
+	//Description: Gives cordinates to use for placement of debris and player classes
+	//Precondition: takes in two values and gives a X & Y axis for placement
+	//Post Condition: returns the positon 
+	//Protection Level: Public
 	Vector2 mPos;
 };
--- project2D/Player2D.h
+++ project2D/Player2D.h
@@ -5,28 +5,28 @@
 class Player2D : public Entity
 {
 public:
-	//Prototype:
-	//Description:
-	//Precondition:
-	//Post Condition:
-	//Protection Level:
+	//Prototype: Player2D()
+	//Description: Default constructor for player variables needed for game functions
+	//Precondition: Sets the position and size of the ship to be feed in to the game
+	//Post Condition: returns the set value when called in the function
+	//Protection Level: Public
 	Player2D();
-	//Prototype:
-	//Description:
-	//Precondition:
-	//Post Condition:
-	//Protection Level:
+	//Prototype: ~Player2D()
+	//Description: Deconstructs the Player variables for Player2D functionality
+	//Precondition: Takes in the functionality of the default constructor
+	//Post Condition: Frees ram in the default
+	//Protection Level: Public
 	~Player2D();
-	//Prototype:
-	//Description:
-	//Precondition:
-	//Post Condition:
-	//Protection Level:
+	//Prototype: void Update(float deltaTime)
+	//Description: Updates the player's position, speed, and boundries
+	//Precondition: gives the player's ship its position, speed, and boundries
+	//Post Condition: Feeds the information into the main function and allows movement properties
+	//Protection Level: Public
 	void Update(float deltaTime);
-	//Prototype:
-	//Description:
-	//Precondition:
-	//Post Condition:
-	//Protection Level:
+	//Prototype: bool isAlive
+	//Description: the condition controls the losing condition when the player is dead it causes game over status
+	//Precondition: checks to see if the player is still alive
+	//Post Condition: if the player doesnt make the time limit and dies, game over
+	//Protection Level: Public
 	bool isAlive;
 };
